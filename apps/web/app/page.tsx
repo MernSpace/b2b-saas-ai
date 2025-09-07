264b39b5aefe1a9f72f5510b8a20446fb2700414
mernspace <sifat355y@gmail.com>
2025-09-07 17:25:54 +0600

    mutation added

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,18 +1,18 @@
 "use client"
 import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math/add"
 import { api } from "@workspace/backend/_generated/api"
-import { useQuery } from "convex/react"
+import { useMutation, useQuery } from "convex/react"
 
 export default function Page() {
   const users = useQuery(api.users.gentMany)
+  const add = useMutation(api.users.add)
 
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
-        {add(5, 5)}
+        <Button onClick={() => add} size="sm">Button</Button>
+
         {JSON.stringify(users)}
       </div>
     </div>
