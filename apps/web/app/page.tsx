e35c1e209af140ee3ac43309a5f292d86885a580
mernspace <sifat355y@gmail.com>
2025-09-07 17:19:15 +0600

    02:convex pagckage

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,19 @@
+"use client"
 import { Button } from "@workspace/ui/components/button"
 import { add } from "@workspace/math/add"
+import { api } from "@workspace/backend/_generated/api"
+import { useQuery } from "convex/react"
 
 export default function Page() {
+  const users = useQuery(api.users.gentMany)
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
         <Button size="sm">Button</Button>
         {add(5, 5)}
+        {JSON.stringify(users)}
       </div>
     </div>
   )
