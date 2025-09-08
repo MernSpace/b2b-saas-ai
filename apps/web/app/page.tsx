22a8e16658a7b769bbe32dcf184ec965a3bfb9bc
mernspace <sifat355y@gmail.com>
2025-09-08 10:56:18 +0600

    03 clerk authentication

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,20 +1,31 @@
 "use client"
 import { Button } from "@workspace/ui/components/button"
 import { api } from "@workspace/backend/_generated/api"
-import { useMutation, useQuery } from "convex/react"
+import { Authenticated, Unauthenticated, useMutation, useQuery } from "convex/react"
+import { SignInButton, UserButton } from "@clerk/nextjs"
 
 export default function Page() {
   const users = useQuery(api.users.gentMany)
   const add = useMutation(api.users.add)
 
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button onClick={() => add} size="sm">Button</Button>
+    <>
+      <Authenticated>
 
-        {JSON.stringify(users)}
-      </div>
-    </div>
+        <div className="flex items-center justify-center min-h-svh">
+          <UserButton />
+          <div className="flex flex-col items-center justify-center gap-4">
+            <h1 className="text-2xl font-bold">Hello World</h1>
+            <Button onClick={() => add} size="sm">Button</Button>
+
+            {JSON.stringify(users)}
+          </div>
+        </div>
+      </Authenticated>
+      <Unauthenticated>
+        <p>Please login</p>
+        <SignInButton />
+      </Unauthenticated>
+    </>
   )
 }
